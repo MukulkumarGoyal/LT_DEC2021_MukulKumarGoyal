3255a0948014b05fcbed8760059a9cbc865414e2
Mukul-Kumar-Goyal <mukulkumargoyal18@gmail.com>
2021-12-28 01:42:23 +0530

    Third Commit

--- logic.js
+++ logic.js
@@ -15,5 +15,5 @@ function carousel() {
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
   y[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 1000); // Change image every 2 seconds
+  setTimeout(carousel, 5000); // Change image at every 2 seconds
 }
